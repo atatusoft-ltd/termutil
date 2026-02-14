02584263f6f90189d2a76b0769d636d57336b9bc
Andrew Masiye <amasiye313@gmail.com>
2026-02-14 11:47:35 +0200

    feat: add enumerations for horizontal and vertical alignment, and window position with coordinate calculation

---
+++ src/UI/Windows/Enumerations/VerticalAlignment.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Atatusoft\Termutil\UI\Windows\Enumerations;
+
+/**
+ * Enumeration for vertical alignment. This is used to determine how the content of a window should be aligned vertically.
+ *
+ * @package Sendama\Console\UI\Windows\Enumerations
+ */
+enum VerticalAlignment
+{
+    case TOP;
+    case MIDDLE;
+    case BOTTOM;
+}
