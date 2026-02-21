34853743c16f3d622f78c8adf740dd0025146111
Andrew Masiye <amasiye313@gmail.com>
2026-02-21 23:43:57 +0200

    feat(io): create MouseEvent.php
    * Add methods to Console.php to enable/disbale mouse event reporting.

---
+++ src/IO/Enumerations/MouseTrackingMode.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Atatusoft\Termutil\IO\Enumerations;
+
+/**
+ * An enumeration of ANSI mouse reporting modes for capturing mouse input in a terminal.
+ * Once enabled the terminal will send input sequences to your program's stdin whenever a
+ * mouse event occurs.
+ */
+enum MouseTrackingMode: int
+{
+    case STANDARD_CLICK = 1000;
+    case CELL_MOTION_TRACKING = 1002;
+    case ALL_MOTION_TRACKING = 1003;
+
+    /**
+     * Returns the ANSI escape sequence for the corresponding mode.
+     *
+     * @param bool $withSGRExtendedMode
+     * @return string
+     */
+    public function getSequence(bool $withSGRExtendedMode = true): string
+    {
+        $sequence = "\003[?{$this->value}";
+
+        if ($withSGRExtendedMode) {
+            $sequence .= ";1006";
+        }
+
+        return "{$sequence}h";
+    }
+}
---
+++ src/IO/Mouse/Enumerations/MouseButton.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Atatusoft\Termutil\IO\Mouse\Enumerations;
+
+enum MouseButton: int
+{
+    case LEFT_BUTTON = 32;
+    case MIDDLE_BUTTON = 33;
+    case RIGHT_BUTTON = 34;
+    case RELEASED = 35;
+}
